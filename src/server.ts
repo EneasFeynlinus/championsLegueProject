ce1fc6f44d82cd67cdeade86dea390c78b69c0a6
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-14 00:35:39 -0300

    setting port to test API with thunder cliend with defined http route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello World")
+import express, {Request, response, Response} from "express";
+
+const app = express();
+
+app.get("/", (request: Request, response: Response) => {
+    response.send("Hello world")
+})
+
+app.listen(3333);
