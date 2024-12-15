6f85480c1ec57c80d0eba6f347673204ca0f1792
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-15 12:37:03 -0300

    Defining repository layer, setting interface to be attributed do its functions

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -7,3 +7,11 @@ const database: PlayerModel[] = [
     {id: 1, name: "Ronaldo"},
     {id: 2, name: "Messi"},
 ]
+
+export const findAllPlayers = async (): Promise<PlayerModel[]> => {
+    return database;
+}
+
+export const findPlayerById = async (id: number): Promise<PlayerModel | undefined> => {
+    return database.find((player) => player.id === id);
+}
